be9a2107c0140202bbe6231b9251301b47187a77
Ghazi <mohdghazi96@gmail.com>
2025-07-01 22:31:04 +0800

    Create route for Product

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 //Route::get('/', function () {
@@ -8,3 +9,5 @@ use Illuminate\Support\Facades\Route;
 //});
 
 Route::get('/', DashboardController::class)->name('dashboard');
+
+Route::resource('products', ProductController::class);
