d633df2658fb5efa7ba9a3e54ef32f78d1275b82
Ghazi <mohdghazi96@gmail.com>
2025-07-02 03:00:03 +0800

    Run laravel pint

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,9 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-//Route::get('/', function () {
+// Route::get('/', function () {
 //    return view('welcome');
-//});
+// });
 
 Route::get('/', DashboardController::class)->name('dashboard');
 
